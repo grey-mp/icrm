91e6696d1d655048f2e6c8b1b28f2d73f9974f77
maurizio <maurizio@192.168.16.72>
2014-02-05 22:58:18 +0100

    Test buttons

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,3 +6,22 @@ $this->pageTitle = Yii::app()->name . ' - Home';
 ?>
 
 <h1><?php echo CHtml::encode(Yii::app()->name); ?> - Home page</h1>
+<p>
+    <button class="btn btn-default">default</button>
+    <button class="btn btn-primary">primary</button>
+    <button class="btn btn-danger">danger</button>
+    <button class="btn btn-info">info</button>
+    <button class="btn btn-success">success</button>
+    <button class="btn btn-warning">warning</button>
+</p>
+<p>
+    <button class="btn btn-default btn-lg">default lg</button>
+    <button class="btn btn-default">default</button>
+    <button class="btn btn-default btn-sm">default sm</button>
+    <button class="btn btn-default btn-xs">default xs</button>
+</p>
+<div style="width: 50%;">
+    <button class="btn btn-default btn-lg btn-block">default lg block</button>
+    <button class="btn btn-primary btn-lg btn-block" disabled="disabled">default lg block disabled</button>
+    <button class="btn btn-primary btn-lg btn-block active">default lg block active</button>
+</div>
